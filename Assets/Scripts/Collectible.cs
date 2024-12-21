a3ea4de9561cf9c37ab36026c0379e2ff0f7bee6
Tasha Hankewych <thankewych@gmail.com>
2024-12-20 22:17:19 -0600

    Add sound effect when collecting candy canes

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [SerializeField] AudioClip collectibleSound;
+    
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player")
         {
             other.gameObject.GetComponent<PlayerController>().IncreaseSpeed();
+            other.gameObject.GetComponent<AudioSource>().PlayOneShot(collectibleSound);
             Destroy(gameObject);
         }
     }
